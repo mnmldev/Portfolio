59b6bc1fb7c95033008b8ea90b58d8e47589f71b
Alex Loewe <aleloewe@gmail.com>
2016-04-29 22:16:04 +0200

    yea

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/public'));
 
